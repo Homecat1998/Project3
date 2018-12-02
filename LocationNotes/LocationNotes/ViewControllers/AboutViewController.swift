3305c75de5e8a319d0170b51f5d498f842b8565d
Zhong <zzhong9@itc-imac-15.ur.rochester.edu>
2018-12-02 15:34:20 -0500

    just one eleft

---
+++ LocationNotes/LocationNotes/ViewControllers/AboutViewController.swift
@@ -0,0 +1,52 @@
+//
+//  AboutViewController.swift
+//  LocationNotes
+//
+//  Created by Zhong, Zhetao on 12/2/18.
+//  Copyright © 2018 Zhong, Zhetao. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    let defaults = UserDefaults(suiteName: kAppGroupBundleID)!
+    
+    @IBOutlet weak var appName: UILabel!
+    @IBOutlet weak var appVer: UILabel!
+    @IBOutlet weak var appBuild: UILabel!
+    @IBOutlet weak var launchDate: UILabel!
+    @IBOutlet weak var launchNum: UILabel!
+    @IBOutlet weak var copyRight: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        appName.text = Bundle.main.displayName
+        appVer.text = Bundle.main.version
+        appBuild.text = Bundle.main.build
+        copyRight.text = Bundle.main.copyright
+        launchNum.text = defaults.integer(forKey: dNumLaunches).description
+        launchDate.text = defaults.string(forKey: dAccessDate)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
